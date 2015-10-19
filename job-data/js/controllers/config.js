4d2c2ba0f8e913c0aaf5a72eeb277a521a0aefda
vishal <vishal@qburst.com>
2015-10-19 12:00:35 +0530

    dev

--- job-data/js/controllers/config.js
+++ job-data/js/controllers/config.js
@@ -4,30 +4,18 @@ app.config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $u
         .state("login", {
             url : "/",
             templateUrl : "templates/login.html",
-            controller : "loginController"
+            controller : "loginController as login"
         })
         .state("home", {
             url : "/home",
             templateUrl : "templates/home.html",
-            controller : "homeController"
+            controller : "homeController as home"
         })
         .state("details", {
             url : "/details",
             templateUrl : "templates/details.html",
-            controller : "detailsController"
+            controller : "detailsController as details"
         });
     $urlRouterProvider.otherwise("/");
 }]);
 
-//app.run(["$rootScope","$location","$state","users", function($rootScope,$location,$state,users){
-//    $rootScope.$on("$stateChangeStart", function(event, toState, toParams, fromState, fromParams) {
-//        if (users.loggedin==="false"  && toState.url==="/home") {
-//            event.preventDefault();
-//            $location.path("/");
-//        } else if(users.loggedin==="false" && toState.url==="/details"){
-//            event.preventDefault();
-//            $location.path("/");
-//       }
-//    });
-//
-//}]);
